4855979c2e53a47f54608ced5d20d79115cc553d
eswar2001 <teswar2001@yahoo.co.in>
2020-05-22 21:07:32 +0530

    bug fix

--- whatsapp.py
+++ whatsapp.py
@@ -21,7 +21,7 @@ with open('message.txt') as f:
 no_of_message = 1
 recipetents_list = []
 
-doc_filename = 'NULL'
+doc_filename = 'a.txt'
 
 file_name = "numbers.csv"
 try:
@@ -75,8 +75,7 @@ def send_files():
         # '#app > div > div > div.YD4Yw > div._1-iDe.Wu52Z > span > div > span > div > div > div._2sNbV._3ySAH > span > div > div'
         '/html/body/div[1]/div/div/div[2]/div[2]/span/div/span/div/div/div[2]/span/div/div')
     whatsapp_send_button.click()
-    element_presence(
-        By.XPATH, '//*[@id="main"]/div[3]/div/div/div[2]/div[8]/div/div[1]/div/a/div/div[3]/span', 30)
+    
 
 
 def send_whatsapp_msg(phone_no, text):
